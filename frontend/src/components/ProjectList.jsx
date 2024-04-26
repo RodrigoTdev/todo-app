a8250d4c3410980bb5646c62da49ff91e9422e8c
RodrigoTdev <rodrigotobiodev@gmail.com>
2024-04-26 18:55:53 -0300

    Basic frontend styles

--- frontend/src/components/ProjectList.jsx
+++ frontend/src/components/ProjectList.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export const ProjectList = ({ dataDB, setDataDB }) => {
   return (
     <aside className='project-list'>
+      <h2>PROYECTS</h2>
       <ul>
         {dataDB &&
           dataDB.map((project) => <li key={project._id}>{project.title}</li>)}
