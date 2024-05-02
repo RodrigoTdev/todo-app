f3371072c30433c76e755ddcd094e61e2dc930cb
RodrigoTdev <rodrigotobiodev@gmail.com>
2024-05-01 22:33:23 -0300

    Edit modes in progress

--- frontend/src/components/ProjectList.jsx
+++ frontend/src/components/ProjectList.jsx
@@ -8,6 +8,7 @@ export const ProjectList = ({
   setCurrentProject,
 }) => {
   const [addMode, setAddMode] = useState(false)
+  const [deleteMode, setDeleteMode] = useState(false)
   const [editMode, setEditMode] = useState(false)
   const [draggedItem, setDraggedItem] = useState(null)
   const [items, setItems] = useState()
@@ -37,7 +38,7 @@ export const ProjectList = ({
       body: JSON.stringify({ _id: id }),
     })
     setReRender(!reRender)
-    setEditMode(false)
+    setDeleteMode(false)
   }
 
   // Drag and Drop
@@ -88,15 +89,15 @@ export const ProjectList = ({
               onDragEnd={handleDragEnd}
             >
               {project.title}
-              {editMode && (
+              {deleteMode && (
                 <button
                   onClick={() => handleClickDeleteProject(event, project._id)}
                   className='delete-project'
                 >
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
-                    width='19'
-                    height='19'
+                    width='17'
+                    height='17'
                     viewBox='0 0 16 16'
                   >
                     <path
@@ -141,22 +142,43 @@ export const ProjectList = ({
           </div>
         )}
       </ul>
-      <button
-        onClick={() => setEditMode(!editMode)}
-        className='edit-project-list'
-      >
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          width='20'
-          height='20'
-          viewBox='0 0 24 24'
+      <div className='footer-buttons'>
+        <button className='edit-mode-projets'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='22'
+            height='22'
+            viewBox='0 0 24 24'
+          >
+            <path
+              fill='#059669'
+              d='M5.615 20q-.69 0-1.152-.462Q4 19.075 4 18.385V5.615q0-.69.463-1.152Q4.925 4 5.615 4h7.18q.25 0 .374.159t.125.341q0 .183-.128.341q-.128.159-.378.159H5.615q-.23 0-.423.192Q5 5.385 5 5.615v12.77q0 .23.192.423q.193.192.423.192h12.77q.23 0 .423-.192q.192-.193.192-.423V11.11q0-.25.159-.375t.341-.125q.183 0 .341.125t.159.375v7.275q0 .69-.462 1.152q-.463.463-1.153.463zM10 13.192v-1.136q0-.323.13-.628q.132-.305.349-.522l8.465-8.466q.166-.165.348-.228q.183-.064.385-.064q.188 0 .368.064q.18.063.326.21L21.483 3.5q.16.165.242.364q.083.2.083.401t-.06.382q-.061.18-.227.345l-8.523 8.523q-.217.218-.522.351q-.305.134-.628.134h-1.04q-.348 0-.578-.23q-.23-.23-.23-.578m10.813-8.907l-1.111-1.17zM11 13h1.092l6.666-6.665l-.546-.547l-.61-.584L11 11.806zm7.212-7.212l-.61-.584zl.546.547z'
+            />
+          </svg>
+        </button>
+        <button
+          onClick={() => setDeleteMode(!deleteMode)}
+          className='delete-mode-projects'
         >
-          <path
-            fill='#000000'
-            d='M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h6.525q.5 0 .75.313t.25.687q0 .375-.262.688T11.5 5H5v14h14v-6.525q0-.5.313-.75t.687-.25q.375 0 .688.25t.312.75V19q0 .825-.587 1.413T19 21zm4-7v-2.425q0-.4.15-.763t.425-.637l8.6-8.6q.3-.3.675-.45t.75-.15q.4 0 .763.15t.662.45L22.425 3q.275.3.425.663T23 4.4q0 .375-.137.738t-.438.662l-8.6 8.6q-.275.275-.637.438t-.763.162H10q-.425 0-.712-.288T9 14m12.025-9.6l-1.4-1.4zM11 13h1.4l5.8-5.8l-.7-.7l-.725-.7L11 11.575zm6.5-6.5l-.725-.7zl.7.7z'
-          />
-        </svg>
-      </button>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='19'
+            height='19'
+            viewBox='0 0 16 16'
+          >
+            <path
+              fill='#e11d48'
+              fillRule='evenodd'
+              d='M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16'
+              clipRule='evenodd'
+            />
+            <path
+              fill='#e11d48'
+              d='M11.854 4.854a.5.5 0 0 0-.707-.707L8 7.293L4.854 4.147a.5.5 0 1 0-.707.707L7.293 8l-3.146 3.146a.5.5 0 0 0 .707.708L8 8.707l3.147 3.147a.5.5 0 0 0 .707-.708L8.708 8z'
+            />
+          </svg>
+        </button>
+      </div>
     </aside>
   )
 }
